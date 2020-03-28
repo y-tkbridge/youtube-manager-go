165228aa12bd412e4b375e53635163c16ef3222d
y-tkbridge <novsleep@outlook.jp>
2020-03-29 01:17:25 +0900

    修正内容の反映

--- web/routes/api.go
+++ web/routes/api.go
@@ -2,7 +2,11 @@ package routes
 
 import (
 	"github.com/labstack/echo"
+<<<<<<< HEAD
 	"github.com/y-tkbridge/youtube-manager-go/web/api"
+=======
+	"github.com/y-bridge/youtube-manager-go/web/api"
+>>>>>>> 修正内容の反映
 )
 
 func Init(e *echo.Echo) {
