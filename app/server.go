ea8d2c6966a75fe9635d2c51495771bb1722f017
y-tkbridge <novsleep@outlook.jp>
2020-03-29 00:53:42 +0900

    non

--- app/server.go
+++ app/server.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"github.com/labstack/echo"
-	"github.com/y-tkbridge/youtube-manager-go/web/api"
+	"github.com/y-tkbridge/youtube-manager-go/web/routes"
 )
 
 func main() {
 	e := echo.New()
-	routes.Inite(e)
+	routes.Init(e)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
